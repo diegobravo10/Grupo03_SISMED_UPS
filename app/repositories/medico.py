f96e6adaeba1057b7b01ee7e24e32ecf796ea5e2
Rafael Serrano <rafsantiserrano@hotmail.com>
2026-06-17 19:53:52 -0500

    implementacion de repository en medico

--- app/repositories/medico.py
+++ app/repositories/medico.py
@@ -1,7 +1,32 @@
-from sqlalchemy.orm import Session
-
 from app.models.medico import Medico
 
 
-def obtener_por_id(db: Session, medico_id: int) -> Medico | None:
-    return db.query(Medico).filter(Medico.id == medico_id).first()
+class MedicoRepository:
+
+    @staticmethod
+    def obtener_todos(db):
+        return db.query(Medico).all()
+
+    @staticmethod
+    def obtener_por_id(db, medico_id):
+        return db.query(Medico).filter(
+            Medico.id == medico_id
+        ).first()
+
+    @staticmethod
+    def crear(db, medico):
+        db.add(medico)
+        db.commit()
+        db.refresh(medico)
+        return medico
+
+    @staticmethod
+    def actualizar(db, medico):
+        db.commit()
+        db.refresh(medico)
+        return medico
+
+    @staticmethod
+    def eliminar(db, medico):
+        db.delete(medico)
+        db.commit()
