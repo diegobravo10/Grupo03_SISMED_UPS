0e1701dfddb743c08643b1a6df0519b5b2e57c75
Rafael Serrano <rafsantiserrano@hotmail.com>
2026-06-17 19:00:03 -0500

    definicion de funciones para pacientes

--- app/repositories/paciente.py
+++ app/repositories/paciente.py
@@ -1,7 +1,26 @@
-from sqlalchemy.orm import Session
-
 from app.models.paciente import Paciente
 
+class PacienteRepository:
 
-def obtener_por_id(db: Session, paciente_id: int) -> Paciente | None:
-    return db.query(Paciente).filter(Paciente.id == paciente_id).first()
+    @staticmethod
+    def obtener_todos(db):
+        return db.query(Paciente).all()
+
+    @staticmethod
+    def obtener_por_id(db, paciente_id):
+        return db.query(Paciente).filter(
+            Paciente.id == paciente_id
+        ).first()
+
+    @staticmethod
+    def obtener_por_cedula(db, cedula):
+        return db.query(Paciente).filter(
+            Paciente.cedula == cedula
+        ).first()
+
+    @staticmethod
+    def crear(db, paciente):
+        db.add(paciente)
+        db.commit()
+        db.refresh(paciente)
+        return paciente
